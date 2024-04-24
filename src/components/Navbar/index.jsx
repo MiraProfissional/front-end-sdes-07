6e2dcc48dd19202e700837390cad0712a3ba5c52
MiraProfissional <jvitor.gmira@gmail.com>
2024-04-24 19:57:31 -0300

    Finalização do componente 'Navbar'

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,28 +1,36 @@
 import { Link } from 'react-router-dom'
 import imagem from '../../images/logo-claro-trakmaq.png'
 import './styles.css'
+import { CiHome, CiUser, CiShop, CiPhone, CiShoppingBasket } from "react-icons/ci";
+
 
 function Navbar() {
     return (
         <nav className='navbar'>
-            <div>
-                <Link to="/">
-                    <img src={imagem} alt="logo-trakmaq" /> 
-                </Link>
-                <ul className='lista-nav'>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>Sobre</li>
-                    <li>Contato</li>
-                    <li>
-                        <Link to="/produtos">Produtos</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                </ul>
+            <div className='div-img'>
+            <Link to="/">
+                <img src={imagem} alt="logo-trakmaq" /> 
+            </Link> 
             </div>
+                
+            <ul className='lista-nav'>
+                <li>
+                    <Link to="/"><CiHome/> Home</Link>
+                </li>
+                <li>
+                    <Link to="/"><CiShop/> Sobre Nós </Link>
+                </li>
+                <li>
+                    <Link to="/"><CiPhone/> Contato</Link>
+                </li>
+                <li>
+                    <Link to="/produtos"><CiShoppingBasket/> Produtos</Link>
+                </li>
+                <li>
+                    <Link to="/login"><CiUser/> Login</Link>
+                </li>
+            </ul>
+    
         </nav>
     )
 }
