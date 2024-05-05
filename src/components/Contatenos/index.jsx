71b419d379d572b7c197f088d0c1920965cfc7f4
MiraProfissional <jvitor.gmira@gmail.com>
2024-05-05 20:17:26 -0300

    Ajustes para CI: Babel e Imagens

--- src/components/Contatenos/index.jsx
+++ src/components/Contatenos/index.jsx
@@ -13,7 +13,7 @@ export function Contatenos() {
                         <li><CiMobile4 /> (12) 99783-3901</li>
                         <li><CiMail /> vendas@traqmak.com.br</li>
                         <li><CiLocationOn /> Rua Araguaia, 70 - Vila São Bento, São José dos Campos - São Paulo</li>
-                        <li><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3666.6251951766358!2d-45.8787412!3d-23.220325999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cc4aee5c000207%3A0xfd4d793e2010dc9e!2sR.%20Araguaia%2C%2070%20-%20Vila%20Sao%20Bento%2C%20S%C3%A3o%20Jos%C3%A9%20dos%20Campos%20-%20SP%2C%2012231-380!5e0!3m2!1spt-BR!2sbr!4v1714580367181!5m2!1spt-BR!2sbr" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></li>
+                        <li><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3666.6251951766358!2d-45.8787412!3d-23.220325999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cc4aee5c000207%3A0xfd4d793e2010dc9e!2sR.%20Araguaia%2C%2070%20-%20Vila%20Sao%20Bento%2C%20S%C3%A3o%20Jos%C3%A9%20dos%20Campos%20-%20SP%2C%2012231-380!5e0!3m2!1spt-BR!2sbr!4v1714580367181!5m2!1spt-BR!2sbr" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="Mapa da localização da Trakmaq"></iframe></li>
                         <li><CiClock2 /> Segunda à Sexta-Feira das 8:30h as 17:30h / Sábado das 8:30h às 12h</li>
                     </ul>
                 </div>
--- src/components/SobreNos/index.jsx
+++ src/components/SobreNos/index.jsx
@@ -5,10 +5,10 @@ export function SobreNos () {
     return (
         <div className='sobre-nos-container'>
             <div className='sobre-nos-img'>
-                <img src={imagem} className='background-img' alt="background-image" />
+                <img src={imagem} className='background-img' alt="yellow-background" />
                 <img src="https://th.bing.com/th/id/OIP.KPwLL2a-vw67al0B_rGMQAHaE7?rs=1&pid=ImgDetMain" 
                 className='team-img'
-                alt="team-img" />
+                alt="team-trakamaq" />
             </div>
             <div className='sobre-nos-text'>
                 <h3 className='main-title-white'>Sobre nós</h3>
