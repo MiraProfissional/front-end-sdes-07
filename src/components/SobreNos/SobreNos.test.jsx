b81e630c3e6c9c2e4ebd8f74790a147de13d14e4
MiraProfissional <jvitor.gmira@gmail.com>
2024-05-06 15:41:48 -0300

    Testes para o componente 'SobreNos'

--- src/components/SobreNos/SobreNos.test.jsx
+++ src/components/SobreNos/SobreNos.test.jsx
@@ -1,11 +1,28 @@
-import { screen } from '@testing-library/react';
-import SobreNos from '.';
+import { render, screen } from '@testing-library/react';
+import SobreNos from './';
 
 describe('<SobreNos />', () => {
-  it('should render de elements', () => {
-    // render (
-    //     <SobreNos />
-    // )
-    // expect(screen.findAllByLabelText('img')).toBeInTheDocument();
+  it('should render SobreNos component without crashing', () => {
+    render(<SobreNos />);
+    
+    expect(screen.getByText('Sobre nós')).toBeInTheDocument();
   });
+
+  it('should render main elements correctly', () => {
+    render(<SobreNos />);
+    
+    expect(screen.getByAltText('yellow-background')).toBeInTheDocument();
+    expect(screen.getByAltText('team-trakamaq')).toBeInTheDocument();
+    expect(screen.getByText(/Sed ut perspiciatis/i)).toBeInTheDocument();
+  });
+
+  it('should load images correctly', () => {
+    render(<SobreNos />);
+    
+    const backgroundImage = screen.getByAltText('yellow-background');
+    const teamImage = screen.getByAltText('team-trakamaq');
+    expect(backgroundImage).toHaveAttribute('src', expect.stringContaining('.png'));
+    expect(teamImage).toHaveAttribute('src', expect.stringContaining('https://th.bing.com/th/id/OIP.KPwLL2a-vw67al0B_rGMQAHaE7?rs=1&pid=ImgDetMain'));
+  });
+
 });
