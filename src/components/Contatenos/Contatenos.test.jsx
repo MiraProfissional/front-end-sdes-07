ff144ae2082608c73c63f99ff3b65e6e5d87d6f5
MiraProfissional <jvitor.gmira@gmail.com>
2024-05-20 16:18:36 -0300

    Correcao no teste do componente 'FormularioContato'

--- src/components/Contatenos/Contatenos.test.jsx
+++ src/components/Contatenos/Contatenos.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Contatenos from './';
 
 describe('<Contatenos />', () => {
@@ -17,31 +17,5 @@ describe('<Contatenos />', () => {
         'Segunda à Sexta-Feira das 8:30h as 17:30h / Sábado das 8:30h às 12h',
       ),
     ).toBeInTheDocument();
-  });
-
-  it('should handle form input correctly', () => {
-    render(<Contatenos />);
-
-    fireEvent.change(screen.getByLabelText('Nome'), {
-      target: { value: 'João' },
-    });
-    fireEvent.change(screen.getByLabelText('Email'), {
-      target: { value: 'joao@example.com' },
-    });
-    fireEvent.change(screen.getByLabelText('Assunto'), {
-      target: { value: 'Dúvida sobre produto' },
-    });
-    fireEvent.change(screen.getByLabelText('Mensagem'), {
-      target: { value: 'Tenho uma dúvida sobre o produto X.' },
-    });
-
-    expect(screen.getByLabelText('Nome')).toHaveValue('João');
-    expect(screen.getByLabelText('Email')).toHaveValue('joao@example.com');
-    expect(screen.getByLabelText('Assunto')).toHaveValue(
-      'Dúvida sobre produto',
-    );
-    expect(screen.getByLabelText('Mensagem')).toHaveValue(
-      'Tenho uma dúvida sobre o produto X.',
-    );
   });
 });
--- src/components/FormularioContato/FormularioContato.test.jsx
+++ src/components/FormularioContato/FormularioContato.test.jsx
@@ -1,3 +1,32 @@
+import FormularioContato from './';
+import { render, screen, fireEvent } from '@testing-library/react';
+
 describe('<FormularioContato />', () => {
   it('should render', () => {});
+
+  it('should handle form input correctly', () => {
+    render(<FormularioContato />);
+
+    fireEvent.change(screen.getByLabelText('Nome:'), {
+      target: { value: 'João' },
+    });
+    fireEvent.change(screen.getByLabelText('Email:'), {
+      target: { value: 'joao@example.com' },
+    });
+    fireEvent.change(screen.getByLabelText('Assunto:'), {
+      target: { value: 'Dúvida sobre produto' },
+    });
+    fireEvent.change(screen.getByLabelText('Mensagem:'), {
+      target: { value: 'Tenho uma dúvida sobre o produto X.' },
+    });
+
+    expect(screen.getByLabelText('Nome:')).toHaveValue('João');
+    expect(screen.getByLabelText('Email:')).toHaveValue('joao@example.com');
+    expect(screen.getByLabelText('Assunto:')).toHaveValue(
+      'Dúvida sobre produto',
+    );
+    expect(screen.getByLabelText('Mensagem:')).toHaveValue(
+      'Tenho uma dúvida sobre o produto X.',
+    );
+  });
 });
